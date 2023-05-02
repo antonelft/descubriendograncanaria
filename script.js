f4a73052da2b482c5d21719ce50958d1d5ad1e01
antonelft <47756946+antonelft@users.noreply.github.com>
2023-05-02 10:07:12 +0100

    update 02/05/2023

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
 function destruir() {
   
-  document.getElementById("grancan").style.display = "none";
-  document.getElementById("nuevolug").style.display = "none";
-  document.getElementById("lugares").style.display = "none";
-  document.getElementByClass("para").visibility = "none";
+  document.getElementById("select").style.display = "none";
+  document.getElementById("fm1").style.display = "none";
+  document.getElementById("enviar").style.display = "none";
+  document.getElementById("anadir").style.display = "block";
+  document.getElementById("uploads").style.display = "none";
+  document.getElementById("urlinfo").style.display = "none";
   
-}   
+} 
+
+
